2dba3e84f438595378517c1ae90e9238ff61c020
Ahmed Zafar <153266724+AhmedZafar5533@users.noreply.github.com>
2025-03-22 00:40:17 +0500

    Update vendorStore.js

--- Frontend/Store/vendorStore.js
+++ Frontend/Store/vendorStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { toast } from "sonner";
 
-const baseUrl = "https://online-market-pv34e6md8-ahmeds-projects-c5602810.vercel.app/";
+const baseUrl = "https://online-market-pv34e6md8-ahmeds-projects-c5602810.vercel.app";
 
 export const useVendorStore = create((set) => ({
     vendor: [],
