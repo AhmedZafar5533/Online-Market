e7330e4bad9ff7a3406097edf1e4faafa8edd5b2
Ahmed Zafar <153266724+AhmedZafar5533@users.noreply.github.com>
2025-03-22 05:04:51 +0500

    Update servicesStore.js

--- Frontend/Store/servicesStore.js
+++ Frontend/Store/servicesStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { toast } from "sonner";
 
-const baseUrl = "https://online-market-2dm9.onrender.com/api";
+const baseUrl = "https://online-market-api.vercel.app/api";
 
 // Helper function for API requests with error handling
 const fetchData = async (url, options) => {
